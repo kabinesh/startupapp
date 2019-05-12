b71d8f95cd4159ab2cb07e7b1a8673fb7be0133b
udayaugustin <udayaugustin@gmail.com>
2019-05-12 13:50:44 +0530

    Group Messge View binded to the GroupMessageView model.

---
+++ SmartSchoolApp/SmartSchoolApp/Services/GroupMessageService.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using SmartSchoolApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSchoolApp.Services
+{
+    public class GroupMessageService
+    {
+        private string baseUrl = "http://sms.shanehospitalfurniture.com";
+        private HttpClient httpClient;
+        private string authToken;
+
+        public GroupMessageService()
+        {
+            httpClient = new HttpClient();
+            authToken = "co6m1mo3";
+        }
+
+        public async Task<ObservableCollection<GroupMessage>> GetGroupMessages()
+        {
+            var objectDefnition = new { Status ="", PostDetails = new List<GroupMessage>() };
+            var content = await httpClient.GetStringAsync(baseUrl + "/post/get?AuthToken=" + authToken);
+            var data = JsonConvert.DeserializeAnonymousType(content, objectDefnition);
+    
+           
+            ObservableCollection<GroupMessage> groupMessages = new ObservableCollection<GroupMessage>(data.PostDetails);
+            return groupMessages;
+        }
+    }
+}
